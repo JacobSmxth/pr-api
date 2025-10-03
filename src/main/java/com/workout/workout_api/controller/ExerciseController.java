1fa245704cdd6c4d04eaa1e3c4174f9427be69b3
Jacob Smith <jacob.d.smith@live.com>
2025-10-03 11:22:29 -0400

    Change from maxes to exercises

--- src/main/java/com/workout/workout_api/controller/ExerciseController.java
+++ src/main/java/com/workout/workout_api/controller/ExerciseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/maxes")
+@RequestMapping("/api/exercises")
 public class ExerciseController {
 
     @Autowired
